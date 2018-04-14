e7cbd95f02c4121653e6344d3d0a68d7f1b11b70
Brenton Jenkins <brentonjjenkins@gmail.com>
2018-04-14 11:05:59 -0700

    css improvements

--- public/app.js
+++ public/app.js
@@ -1,88 +1,56 @@
 // Grab the articles as a json
 $.getJSON("/articles", function (data) {
     // For each one
-
-    var articlesZone = $('#articles')
     for (var i = 0; i < data.length; i++) {
       // Display the apropos information on the page
-      var articleDiv = $('<div>');
-      articleDiv.addClass('article-block');
-
-      var articleTitle = $('<p>');
-      articleTitle.addClass('article-title');
-      articleTitle.text(data[i].title);
-
-      var articleImage = $('<img>');
-      articleImage.attr('src', data[i].img);
-      articleImage.addClass('article-img');
-
-      var articleLink = $('<a>');
-      articleLink.attr({'href': data[i].link,
-                        'target': '_blank' });
-      articleLink.addClass('read-article-link');
-      articleLink.text('Read Article');
-
-      var articleSaveLink = $('<a>');
-      articleSaveLink.attr({'href': data[i].title,
-                            'target': '_blank'});
-      articleSaveLink.attr('data-id', data[i].id);
-      articleSaveLink.addClass('save-article-link');
-      articleSaveLink.text('Save Article');
-      
-      articleDiv.append(articleTitle);
-      articleDiv.append(articleImage);
-      articleDiv.append(articleLink);
-      articleDiv.append(articleSaveLink);
-
-     articlesZone.append(articleDiv);
-     
-      // $("#articles").append(
-      //   "<div class='article-block'><p class='article-title' data-id='" + data[i]._id + 
-      //   "'>" + data[i].title + "</p><img class='article-img'" + " src='" + data[i].img + "'/><a href=" + data[i].link + " class='article-link' target='_blank'>" + "Read Article</a><a href=" + data[i].title + " class='article-link' target='_blank'>" + "Save Article</a></div>"
-      // );
-
-      // $("#articles").append(
-      //   "<div class='article-block'><p class='article-title' data-id='" + data[i]._id + "'>" + data[i].title + "</p><img class='article-img'" + " src='" + data[i].img + "'/><a href=" + data[i].link + " class='article-link' target='_blank'>" + "Read Article</a><a href=" + data[i].title + " class='article-link' target='_blank'>" + "Save Article</a></div>"
-      // );
-
-     articlesZone.on("click", '.save-article-link', function () {
-      // Empty the notes from the note section
-      $("#notes").empty();
-      // Save the id from the p tag
-      var thisId = $(this).attr("data-id");
-    
-      // Now make an ajax call for the Article
-      $.ajax({
-          method: "GET",
-          url: "/articles/" + thisId
-        })
-        // With that done, add the note information to the page
-        .then(function (data) {
-          console.log(data);
-          // The title of the article
-          $("#notes").append("<h2>" + data.title + "</h2>");
-          // An input to enter a new title
-          $("#notes").append("<input id='titleinput' name='title' >");
-          // A textarea to add a new note body
-          $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-          // A button to submit a new note, with the id of the article saved to it
-          $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
-    
-          // If there's a note in the article
-          if (data.note) {
-            // Place the title of the note in the title input
-            $("#titleinput").val(data.note.title);
-            // Place the body of the note in the body textarea
-            $("#bodyinput").val(data.note.body);
-          }
-        });
-    });
+      $("#articles").append(
+        "<div class='article-block'><p class='article-title' data-id='" + data[i]._id + "'>" + data[i].title + "</p><img class='article-img'" + " src='" + data[i].img + "'/><a href=" + data[i].link + " class='article-link' target='_blank'>" + "Read Article</a><div data-id='" + data[i]._id + "' class='article-link write-note'>" + "Write Note</div></div>"
+      );
     }
+  });
+  
+  
+  // Whenever someone clicks a p tag
+  $(document).on("click", ".write-note", function () {
+    // Empty the notes from the note section
+    $("#notes").empty();
+
+    $("#notes").css("display", "inline-block");
+    // Save the id from the p tag
+    var thisId = $(this).attr("data-id");
+  
+    // Now make an ajax call for the Article
+    $.ajax({
+        method: "GET",
+        url: "/articles/" + thisId
+      })
+      // With that done, add the note information to the page
+      .then(function (data) {
+        console.log(data);
+        // The title of the article
+        $("#notes").append("<h2>" + data.title + "</h2>");
+        // An input to enter a new title
+        $("#notes").append("<input id='titleinput' name='title' placeholder='Note Title' >");
+        // A textarea to add a new note body
+        $("#notes").append("<textarea id='bodyinput' name='body' placeholder='Note Content'></textarea>");
+        // A button to submit a new note, with the id of the article saved to it
+        $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+  
+        // If there's a note in the article
+        if (data.note) {
+          // Place the title of the note in the title input
+          $("#titleinput").val(data.note.title);
+          // Place the body of the note in the body textarea
+          $("#bodyinput").val(data.note.body);
+        }
+      });
   });
   
   // When you click the savenote button
   $(document).on("click", "#savenote", function () {
     // Grab the id associated with the article from the submit button
+    $("#notes").css("display", "none");
+
     var thisId = $(this).attr("data-id");
   
     // Run a POST request to change the note, using what's entered in the inputs
